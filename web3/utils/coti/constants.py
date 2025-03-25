c4d3bb0d63928209caf594394c228528d6c682ff
Spencer Miller <smiller@coti.io>
2025-03-25 22:22:35 +0200

    mainnet support

--- web3/utils/coti/constants.py
+++ web3/utils/coti/constants.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 class CotiNetwork(Enum):
     TESTNET = "https://testnet.coti.io/rpc"
+    MAINNET = "https://mainnet.coti.io/rpc"
 
 ACCOUNT_ONBOARD_CONTRACT_ADDRESS = "0x536A67f0cc46513E7d27a370ed1aF9FDcC7A5095"
 
